e56d02962911ac1c821aa2dda2d88d7878d08ef8
Ashish Mahindrakar <ashishmahi46@gmail.com>
2020-09-08 22:51:17 +0530

    CUTOM-TRACER: Ashish: Add examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CustomTracer from './customTracer/Tracer';
+import Axios from 'axios';
+const {
+  BatchRecorder
+} = require("zipkin");
+const {
+  HttpLogger
+} = require("zipkin-transport-http");
+const ZipkinJavascriptOpentracing = require("zipkin-javascript-opentracing");
+
+const recorder = new BatchRecorder({
+  logger: new HttpLogger({
+    endpoint: "http://localhost:9411/api/v1/spans"
+  })
+});
+
+const zipkinTracer = new ZipkinJavascriptOpentracing({
+  serviceName: "My Service",
+  recorder,
+  kind: "client"
+});
+
+const onClickOfZipkinTracer = async () => {
+  const span = zipkinTracer.startSpan("ZipkinTracerSpan");
+  span.log({
+  logMsg:"Logging in Zipkin Tracer"
+  });
+  span.setTag("TagInZipkin","Foo")
+  const data = await Axios.get('/');
+  console.log(data.data);
+  span.finish();
+}
+
+const onClickOfCustomTracer = async () => {
+  const customTracer = new CustomTracer({
+    url: "http://localhost:9411/api/v1/spans",
+    interval: 100,
+    serviceName: "Custom Tracer"
+  });
+  const span = customTracer.startSpan("CustomTracerSpan");
+  span.log("Logging in Custom tracer");
+  span.log("Logging again in Custom tracer");
+  span.setTag("TagInZipkin","Foo")
+  const data = await Axios.get('/');
+  console.log(data.data);
+  span.finish();
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+    <div className = "App" >
+    <h1 > Tracing in Zipkin Tracer Vs Custom Tracer </h1>
+     <button onClick={() => onClickOfZipkinTracer()} class = "button button1" > Press To See Zipkin Tracer In Action </button> 
+     <button onClick={() => onClickOfCustomTracer()} class = "button button1" > Press To See Custom Tracer In Action </button> 
+     </div>
   );
 }
 
---
+++ src/customTracer/Span.js
@@ -0,0 +1,52 @@
+/* eslint-disable class-methods-use-this */
+const { uniqueId, getTimeStamp } = require("./helpers");
+
+class Span {
+  constructor(name, callBackOnFinish) {
+    this.name = name;
+    const uID = uniqueId();
+    this.id = uID;
+    this.traceId = uID;
+    this.startTime = Date.now();
+    this.logs = [];
+    this.tags = [];
+    this.callBackOnFinish = callBackOnFinish;
+  }
+
+  setTag(key, value) {
+    this.tags.push({
+      key,
+      value,
+    });
+  }
+
+  log(logString) {
+    this.logs.push({
+      timestamp: getTimeStamp(),
+      value: logString,
+      endpoint: { serviceName: "Custom Tracer" },
+    });
+  }
+
+  // get id() {
+  //   return {
+  //     traceId: this.traceId,
+  //     spanId: this.id
+  //   };
+  // }
+
+  finish() {
+    this.duration = Date.now() - this.startTime;
+    this.callBackOnFinish({
+      name: this.name,
+      duration: this.duration,
+      timestamp: Number(`${this.startTime}000`),
+      traceId: this.traceId,
+      id: this.id,
+      annotations: this.logs,
+      binaryAnnotations: this.tags,
+    });
+  }
+}
+
+module.exports = Span;
